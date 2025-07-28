f7a417aee691a3f66390241d93c1ac1b93c6414a
Chanda Siskin <chanda.siskin@hotmail.com>
2025-07-29 01:42:34 +0200

    Style main-component

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './main.scss'
 })
 export class Main {
-
+  thumbnailSrc = "https://m.media-amazon.com/images/M/MV5BNTBiYWJlMjQtOTIyMy00NTY4LWFhOWItOWZhNzc3NGMyMjc2XkEyXkFqcGc@._V1_QL75_UX90_CR0,0,90,133_.jpg";
+  title = "title";
 }
