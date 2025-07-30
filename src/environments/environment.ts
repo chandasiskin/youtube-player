342371ef3bab86b823910e80e530c96db1f9162c
Chanda Siskin <chanda.siskin@hotmail.com>
2025-07-31 00:46:24 +0200

    Add partial youtube-api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
     apiKey: '',
+    apiUrl: 'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&fields=item(id,snippet(title,thumbnails(default)),contentDetails(duration))',
 };
