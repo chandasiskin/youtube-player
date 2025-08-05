795711ea265a6cd345a6bc35a97518ea2e6e6046
Chanda Siskin <chanda.siskin@hotmail.com>
2025-08-06 00:49:29 +0200

    Move player from footer to main component and add a progress bar

--- src/app/footer/footer.ts
+++ src/app/footer/footer.ts
@@ -1,34 +1,12 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { Playlist } from '../services/playlist';
-import { Video } from '../models/video';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
-export class Footer implements OnInit {
-  private playlistService = inject(Playlist);
-  private sanitizer = inject(DomSanitizer);
-
-  copyrightYear: Number = new Date().getFullYear();
-
-  currentVideoUrl: SafeResourceUrl | null = null;
-  currentVideoTitle: String | null = null;
-
-  ngOnInit(): void {
-    this.playlistService.currentVideo$.subscribe((video: Video | null ) => {
-      if (video) {
-        const url = `https://www.youtube.com/embed/${video.id}?autoplay=1`;
-        this.currentVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
-
-        this.currentVideoTitle = video.title;
-      } else {
-        this.currentVideoUrl = null;
-      }
-    });
-  }
+export class Footer {
+  copyrightYear: number = new Date().getFullYear();
 }
--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Video } from '../models/video';
 import { Playlist } from '../services/playlist';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-main',
@@ -11,14 +12,53 @@ import { CommonModule } from '@angular/common';
 })
 export class Main implements OnInit {
   private playlistService = inject(Playlist);
+  private sanitizer = inject(DomSanitizer);
+
+  private intervalId: any = null;
 
   playlist: Video[] = [];
 
+  currentVideoTitle: string = "";
+  currentVideoUrl: SafeResourceUrl | null = null;
+  currentVideoDuration: number = 0;
+
+  progress: number = 0;
+
   ngOnInit(): void {
     this.playlistService.playlist$.subscribe(data => this.playlist = data);
+    this.playlistService.currentVideo$.subscribe((video: Video | null) => {
+      if (video) {
+        const url = `https://www.youtube.com/embed/${video.id}?autoplay=1`;
+        this.currentVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+        this.currentVideoTitle = video.title;
+        this.currentVideoDuration = video.durationInSeconds;
+      } else {
+        this.currentVideoUrl = null;
+      }
+    })
   }
 
   play(video: Video) {
     this.playlistService.setCurrentVideo(video);
+
+    this.clearMyInterval(this.intervalId);
+
+    let startingTime = Date.now();
+    this.progress = 0;
+
+    this.intervalId = setInterval(() => {
+      if (this.progress >= 100) {
+        this.clearMyInterval(this.intervalId);
+      }
+
+      let elapsedTime = (Date.now() - startingTime) / 1000;
+      let elapsedPercentage = (elapsedTime / this.currentVideoDuration) * 100;
+      this.progress = Math.min(elapsedPercentage, 100);
+    }, 200);
+  }
+
+  private clearMyInterval(intervalId: any): void {
+    clearInterval(intervalId);
+    this.intervalId = null;
   }
 }
