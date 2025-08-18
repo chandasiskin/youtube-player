bb31f524bbf15140efa5336eaf7e46e6dd3e3dea
Chanda Siskin <chanda.siskin@hotmail.com>
2025-08-18 22:59:03 +0200

    [feature] Add gfx for a remove-from-playlist area
    Users will be able to drag songs to this trash-bin, to remove them from the playlist.

--- src/app/main/main.ts
+++ src/app/main/main.ts
@@ -65,6 +65,18 @@ export class Main implements OnInit {
     this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=72MYQo4IUNg");
     this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=yebNIHKAC4A");
     this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=0wjWbYFQgqk");
+      this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=tPEE9ZwTmy0");
+    this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=-FTNbqxCfhA");
+    this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=a3HZ8S2H-GQ");
+    this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=72MYQo4IUNg");
+    this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=yebNIHKAC4A");
+    this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=0wjWbYFQgqk");
+      this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=tPEE9ZwTmy0");
+    this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=-FTNbqxCfhA");
+    this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=a3HZ8S2H-GQ");
+    this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=72MYQo4IUNg");
+    this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=yebNIHKAC4A");
+    this.playlistService.addVideoByUrl("https://www.youtube.com/watch?v=0wjWbYFQgqk");
 
     this.createPlayer();
   }
