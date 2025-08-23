8dfb730849bb9f50ff99fb486c3e117c4f87645f
Chanda Siskin <chanda.siskin@hotmail.com>
2025-08-23 22:26:13 +0200

    [refactor] Add pulsing lights in the background
    Created a method to add 10 pusling lights with random attributes in the background.
    
    Each time the page is refreshed, the lights are randomized (there will not be any page refreshes except the ones forced by the user).

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,35 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Footer } from './footer/footer';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('youtube-player');
+  private numberOfLights: number = 10;
+
+  lights = Array.from({ length: this.numberOfLights }, _ => ({
+    top: `${Math.random() * 90}%`,
+    left: `${Math.random() * 90}%`,
+    size: 100 + Math.random() * 150,
+    color: this.getRandomColor(),
+    delay: `${Math.random() * 10}s`,
+  }));
+
+  private getRandomColor(): string {
+    let hex = "#";
+
+    for (let i = 0; i < 3; i++) {
+      let res = Math.floor(Math.random() * 256).toString(16);
+      hex += res.length === 1 ? '0' + res : res;
+    }
+
+    console.log(hex);
+    return hex;
+  }
 }
